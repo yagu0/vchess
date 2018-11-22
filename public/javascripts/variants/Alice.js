a3eb4cc5131333f1ef08a3f41ba68692a7d6bb63
Benjamin Auder <benjamin.auder@somewhere>
2018-11-22 02:32:33 +0100

    Draft plan for easy Alice chess implementation

--- public/javascripts/variants/Alice.js
+++ public/javascripts/variants/Alice.js
@@ -4,4 +4,9 @@ class AliceRules extends ChessRUles
 	{
 		return ""; //TODO
 	}
+
+	// Idee : this.board assigné tour à tour à board1, board2
+	// board1 initialisé plein, board2 vide (via fen: s,t,u,o,c)
+	// coups cherchés suivant règles normales sur l'un puis l'autre
+	// puis au final filtre.
 }
--- variants.js
+++ variants.js
@@ -5,12 +5,11 @@ module.exports = [
 	{ "name" : "Chess960", "description" : "Standard rules" },
   { "name" : "Antiking", "description" : "Keep antiking in check" },
   { "name" : "Magnetic", "description" : "Laws of attraction" },
-//  { "name" : "Alice", "description" : "Both sides of the mirror" },
+  { "name" : "Alice", "description" : "Both sides of the mirror" },
 //  { "name" : "Grand", "description" : "Big board" },
 //  { "name" : "Wildebeest", "description" : "Balanced sliders & leapers" },
 //  { "name" : "Loser", "description" : "Lose all pieces" },
 //  { "name" : "Crazyhouse", "description" : "Captures reborn" },
 //  { "name" : "Switching", "description" : "Exchange pieces positions" },
 //  { "name" : "Absorption", "description" : "Capture enhance movements" },
-//  { "name" : "Dark", "description" : "In the fog" },
 ];
