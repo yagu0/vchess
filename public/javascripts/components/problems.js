f5d3e4f539355d166baf38bab97480582e90e4af
Benjamin Auder <benjamin.auder@somewhere>
2018-12-15 00:20:15 +0100

    On the road to problems to complete website :)

---
+++ public/javascripts/components/problems.js
@@ -0,0 +1,14 @@
+//TODO: list problems as FEN (quickly rendered), by date, with possible filtering per variant(?)
+//click on a problem ==> land on variant page with mode==friend, FEN prefilled... ok
+
+// get 10 first problems, and buttons next<>previous send date + "before" or "after"
+// need database: sqlite !
+
+// form "new problem" fen(position/turn/flags[guess]), instructions, solution (mandatory)
+// ==> upload on server in sandbox
+//
+// Atomic rules, atomic game, atomic problems(list drawn position
+//   + summary(first chars of instructions) + timedate)... one big Vue ? with components
+//
+// click on problem ==> masque problems, affiche game tab, launch new game Friend with
+//   FEN + turn + flags + rappel instructions / solution on click sous l'échiquier
---
+++ public/javascripts/variant.js
@@ -0,0 +1,7 @@
+new Vue({
+	el: "#variantPage",
+	data: { display: "" }, //do not show anything...
+	// TODO: listen event "show problem", avec le probleme stringifié en arg
+	// Alors: display=game, mode=friend, newGame(fen, turn, ...),
+	//   et set Instructions+Soluce
+});
