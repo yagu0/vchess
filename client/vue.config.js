aa2ccf7d468a268ac58047f8fef62ac64a88d76b
Benjamin Auder <benjamin.auder@somewhere>
2020-02-09 15:11:50 +0100

    Update packages + fix web worker

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  configureWebpack: {
+    output: {
+      // Fix "window is not defined" issues with web worker.
+      // https://github.com/webpack/webpack/issues/6642
+      globalObject: 'this',
+    },
+  },
 };
