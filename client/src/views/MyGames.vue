1aaae6ce703d43ccf4328da9b6b4dca4ed67289a
Benjamin Auder <benjamin.auder@somewhere>
2020-03-04 19:29:03 +0100

    'update'

--- client/src/views/MyGames.vue
+++ client/src/views/MyGames.vue
@@ -43,6 +43,10 @@ export default {
   },
   created: function() {
     GameStorage.getAll(true, localGames => {
+
+
+console.log(localGames);
+
       localGames.forEach(g => (g.type = this.classifyObject(g)));
       this.liveGames = localGames;
     });
