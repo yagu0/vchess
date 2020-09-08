aafc804f005917facbdc85b870d7b109fbac13e5
Benjamin Auder <benjamin.auder@somewhere>
2020-09-08 17:19:26 +0200

    Fix CastleChess

--- client/src/variants/Castle.js
+++ client/src/variants/Castle.js
@@ -4,9 +4,11 @@ export class CastleRules extends ChessRules {
   getCurrentScore() {
     const baseScore = super.getCurrentScore();
     if (baseScore != '*') return baseScore;
-    if (this.getPiece(0,2) == V.KING && this.getPiece(0,3) == V.ROOK)
-      return "0-1";
-    if (this.castleFlags['b'][0] >= 8) return "1-0";
+    if (this.castleFlags['b'][0] >= 8) {
+      if (this.getPiece(0,2) == V.KING && this.getPiece(0,3) == V.ROOK)
+        return "0-1";
+      return "1-0";
+    }
     return '*';
   }
 };
